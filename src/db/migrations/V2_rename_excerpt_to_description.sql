771e3e8328b306b52ed8c3518c4f6f499d1ec191
Josh Ziegler <space-pope@users.noreply.github.com>
2026-07-18 21:40:32 -0400

    remove manual triggers to fix d/b issue
    - add d/b integrity check to startup

--- src/db/migrations/V2_rename_excerpt_to_description.sql
+++ src/db/migrations/V2_rename_excerpt_to_description.sql
@@ -1,10 +1,7 @@
 -- Rename excerpt column to description
 ALTER TABLE entries RENAME COLUMN excerpt TO description;
 
--- Rebuild FTS5 search index to reference 'description' instead of 'excerpt'
-DROP TRIGGER IF EXISTS entries_au;
-DROP TRIGGER IF EXISTS entries_ai;
-DROP TRIGGER IF EXISTS entries_ad;
+-- Rebuild FTS5 search index with auto-sync (no manual triggers)
 DROP TABLE IF EXISTS entries_fts;
 
 CREATE VIRTUAL TABLE IF NOT EXISTS entries_fts USING fts5(
@@ -12,15 +9,3 @@ CREATE VIRTUAL TABLE IF NOT EXISTS entries_fts USING fts5(
     content='entries',
     content_rowid='id'
 );
-
-CREATE TRIGGER entries_ai AFTER INSERT ON entries BEGIN
-    INSERT INTO entries_fts(rowid, title, description) VALUES (new.id, new.title, new.description);
-END;
-
-CREATE TRIGGER entries_ad AFTER DELETE ON entries BEGIN
-    DELETE FROM entries_fts WHERE rowid = old.id;
-END;
-
-CREATE TRIGGER entries_au AFTER UPDATE ON entries BEGIN
-    UPDATE entries_fts SET title = new.title, description = new.description WHERE rowid = new.id;
-END;
